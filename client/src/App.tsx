71acc1cc0da7c4ac162182332e5f785656c08f9a
Ali Marzak <ali.marzak@live.fr>
2025-01-14 00:00:27 +0100

    📦 NEW: Configuration de l'architecture coté client + Installation de la dépendance FontAwesome

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css'
+
+import Header from './layout/Header';
+import Main from './layout/Main';
+import Footer from './layout/Footer';
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <Router>
+      <Header />
+      <Main />
+      <Footer />
+    </Router>
   )
 }
 
---
+++ client/src/configs/api.config.tsx
@@ -0,0 +1,5 @@
+const API_URL = 'https://localhost:3000';
+
+export const API_CONFIG = {
+    baseUrl: API_URL,
+};
